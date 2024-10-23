24c5f8bebb7e05b282e1148fe116417dc140e5f8
laminaMoon <vastodark864@gmail.com>
2024-10-23 09:13:56 -0300

    minhas cartas com perguntas

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'Qual é o conceito de programação?',
+    'termo se refere a uma atividade de planejamento, elaboração, escrita e desenvolvimento de programas,seja de computadores, seja de tablets, celulares ou outros equipamentos eletrônicos'
+)
+
+criaCartao(
+    'Historia',
+    'Qual foi o motivo do início da Primeira Guerra Mundial?',
+    'Primeira Guerra foi o assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austríaco, e sua esposa no dia 28 de junho de 1914'
+)
+
+criaCartao(
+    'Quimica',
+    'O que e ponte salina e qual sua função?',
+    'A ponte salina é um tubo de vidro em forma de U que conecta de forma indireta as soluções do cátodo e do ânodo'
+)
+
+criaCartao(
+    'Fisica',
+    'O que Isaac Newton fez na matemática?',
+    'Em 1665, por exemplo, ele elaborou o teorema binomial, também conhecido como binômio de Newton, teoria que permitiu o desenvolvimento do cálculo, um ramo da matemática'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Grande sonhador em Inglês?',
+    'Grande sonhador em ingles é big dreamer'
+)
